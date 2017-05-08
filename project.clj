f3f413676de05195a21d8d30a1f4cc9451b300af
Toshiki Takeuchi <take@xcoo.jp>
2017-05-08 16:46:54 +0900

    Prepare for next development iteration (0.3.1-SNAPSHOT)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject varity "0.3.0"
+(defproject varity "0.3.1-SNAPSHOT"
   :description "Variant translation library for Clojure"
   :url "https://github.com/chrovis/varity"
   :license {:name "Apache License, Version 2.0"
