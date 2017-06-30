7b1c918d091f78ec8a67282cfba5aef5c5a0e05b
Toshiki Takeuchi <take@xcoo.jp>
2017-06-30 15:02:52 +0900

    Release 0.3.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject varity "0.3.3-SNAPSHOT"
+(defproject varity "0.3.3"
   :description "Variant translation library for Clojure"
   :url "https://github.com/chrovis/varity"
   :license {:name "Apache License, Version 2.0"
