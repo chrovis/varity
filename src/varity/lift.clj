b62529d8be06d42227b8197a143d6f6bcd61d2ab
alumi <alumi@users.noreply.github.com>
2018-04-09 20:39:30 +0900

    Use sorted-map to index chains.

--- src/varity/lift.clj
+++ src/varity/lift.clj
@@ -2,25 +2,14 @@
   "Functions to convert a genome coordinate between assemblies."
   (:require [varity.chain :as ch]))
 
-(defn- calc-diff
-  [rpos data]
-  (loop [[{:keys [^long size ^long dt ^long dq]} & r] data, s 0, d 0]
-    (let [e (+ s size)]
-      (if-not (< rpos s)
-        (if (< rpos e)
-          d
-          (if (some? dt)
-            (recur r (+ e dt) (+ d (- dq dt)))))))))
-
 (defn- convert-coord*
-  [pos {:keys [header data]}]
-  (let [rpos (- pos (:t-start header))]
-    (if-let [d (calc-diff rpos data)]
-      (let [new-pos (+ pos d (- (:q-start header) (:t-start header)))]
-        {:chr (:q-name header)
-         :pos (if (= (:q-strand header) "-")
-                (- (:q-size header) new-pos 1)
-                new-pos)}))))
+  [pos {{:keys [q-name q-strand q-size]} :header
+        {:keys [q-start t-start]} :in-block}]
+  (let [new-pos (+ q-start (- pos t-start))]
+    {:chr q-name
+     :pos (if (= q-strand "-")
+            (inc (- q-size new-pos))
+            new-pos)}))
 
 (defn convert-coord
   "Converts {:chr :pos} between different assemblies, returning a new
