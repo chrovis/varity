3162e06b57633cb557f4e8fcb045073b415d2ea0
Toshiki Takeuchi <take@xcoo.jp>
2018-10-03 17:14:12 +0900

    Update clj-hgvs to 0.2.4

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :min-lein-version "2.7.0"
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/tools.logging "0.4.1"]
-                 [clj-hgvs "0.2.3"]
+                 [clj-hgvs "0.2.4"]
                  [cljam "0.6.0"]
                  [org.apache.commons/commons-compress "1.18"]
                  [proton "0.1.6"]]
