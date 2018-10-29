e62b2bfef03872514c6cf4d7ac41d9e4b29bd8c4
Toshiki Takeuchi <take@xcoo.jp>
2018-10-29 11:56:21 +0900

    VCF->HGVS: add verbose? option

--- src/varity/vcf_to_hgvs.clj
+++ src/varity/vcf_to_hgvs.clj
@@ -1,9 +1,12 @@
 (ns varity.vcf-to-hgvs
   "Functions to convert a VCF-style variant into HGVS."
-  (:require [cljam.io.sequence :as cseq]
+  (:require [clojure.tools.logging :as log]
+            [cljam.io.sequence :as cseq]
             [cljam.io.util :as io-util]
             [cljam.util.chromosome :refer [normalize-chromosome-key]]
+            [proton.string :as pstring]
             [varity.ref-gene :as rg]
+            [varity.vcf-to-hgvs.genome :as genome]
             [varity.vcf-to-hgvs.cdna :as cdna]
             [varity.vcf-to-hgvs.common :refer [normalize-variant]]
             [varity.vcf-to-hgvs.protein :as prot]))
@@ -31,7 +34,7 @@
   (if-let [nvar (normalize-variant var seq-rdr rg)]
     (let [var-start-cds-coord (rg/cds-coord (:pos var) rg)
           var-end-cds-coord (rg/cds-coord (+ (:pos var) (max (count (:ref var)) (count (:alt var)))) rg)
-          nvar-start-cds-coord (rg/cds-coord (:pos nvar) rg) ;; !
+          nvar-start-cds-coord (rg/cds-coord (:pos nvar) rg)
           nvar-end-cds-coord (rg/cds-coord (+ (:pos nvar) (max (count (:ref nvar)) (count (:alt nvar)))) rg)]
       (if (= (:region var-start-cds-coord) (:region nvar-start-cds-coord)
              (:region var-end-cds-coord) (:region nvar-end-cds-coord))
@@ -43,6 +46,25 @@
   [var rg]
   (and (<= (:cds-start rg) (+ (:pos var) (count (:ref var))))
        (<= (:pos var) (:cds-end rg))))
+
+(defn- print-debug-info
+  [var seq-rdr rg]
+  (try
+    (println " variant:" (-> (select-keys var [:chr :pos :ref :alt])
+                             (update :ref pstring/prune 20)
+                             (update :alt pstring/prune 20)
+                             str))
+    (println "ref-gene:" (str (select-keys rg [:name :name2 :strand])))
+    (newline)
+    (println (genome/debug-string var seq-rdr rg))
+    (newline)
+    (println (cdna/debug-string var seq-rdr rg))
+    (when (cds-affected? var rg)
+      (newline)
+      (println (prot/debug-string var seq-rdr rg)))
+    (newline)
+    (catch Exception e
+      (log/warn "Debug printing throws error" e))))
 
 ;;; -> cDNA HGVS
 
@@ -57,7 +79,8 @@
   Options:
 
     :tx-margin  The length of transcription margin, up to a maximum of 10000,
-                default 5000."
+                default 5000.
+    :verbose?   Print debug information, default false."
   {:arglists '([variant ref-seq ref-gene]
                [variant ref-seq ref-gene options])}
   (fn [_ ref-seq ref-gene & _]
@@ -84,15 +107,20 @@
                   (assoc (select-variant {:chr chr, :pos pos, :ref ref, :alt alt}
                                          seq-rdr rg)
                          :rg rg)))
-           (map #(cdna/->hgvs % seq-rdr (:rg %)))
+           (map (fn [{:keys [rg] :as m}]
+                  (when (:verbose? options)
+                    (print-debug-info m seq-rdr rg))
+                  (cdna/->hgvs m seq-rdr rg)))
            distinct)
       (throw (Exception. (format "\"%s\" is not found on %s:%d" ref chr pos))))))
 
 (defmethod vcf-variant->cdna-hgvs :ref-gene-entity
-  [{:keys [pos ref alt]} seq-rdr {:keys [chr] :as rg} & _]
+  [{:keys [pos ref alt]} seq-rdr {:keys [chr] :as rg} & [options]]
   (if (valid-ref? seq-rdr chr pos ref)
     (let [nv (select-variant {:chr chr, :pos pos, :ref ref, :alt alt}
                              seq-rdr rg)]
+      (when (:verbose? options)
+        (print-debug-info nv seq-rdr rg))
       (cdna/->hgvs (assoc nv :rg rg) seq-rdr rg))
     (throw (Exception. (format "\"%s\" is not found on %s:%d" ref chr pos)))))
 
@@ -104,23 +132,28 @@
   allowed. ref-seq must be a path to reference or an instance which implements
   cljam.io.protocols/ISequenceReader. ref-gene must be a path to
   refGene.txt(.gz), ref-gene index, or a ref-gene entity. A returned sequence
-  consists of protein HGVS defined in clj-hgvs."
-  {:arglists '([variant ref-seq ref-gene])}
-  (fn [_ ref-seq ref-gene]
+  consists of protein HGVS defined in clj-hgvs.
+
+  Options:
+
+    :verbose?  Print debug information, default false."
+  {:arglists '([variant ref-seq ref-gene]
+               [variant ref-seq ref-gene options])}
+  (fn [_ ref-seq ref-gene & _]
     (dispatch ref-seq ref-gene)))
 
 (defmethod vcf-variant->protein-hgvs :ref-seq-path
-  [variant ref-seq ref-gene]
+  [variant ref-seq ref-gene & [options]]
   (with-open [seq-rdr (cseq/reader ref-seq)]
-    (doall (vcf-variant->protein-hgvs variant seq-rdr ref-gene))))
+    (doall (vcf-variant->protein-hgvs variant seq-rdr ref-gene options))))
 
 (defmethod vcf-variant->protein-hgvs :ref-gene-path
-  [variant seq-rdr ref-gene]
+  [variant seq-rdr ref-gene & [options]]
   (let [rgidx (rg/index (rg/load-ref-genes ref-gene))]
-    (vcf-variant->protein-hgvs variant seq-rdr rgidx)))
+    (vcf-variant->protein-hgvs variant seq-rdr rgidx options)))
 
 (defmethod vcf-variant->protein-hgvs :ref-gene-index
-  [{:keys [chr pos ref alt]} seq-rdr rgidx]
+  [{:keys [chr pos ref alt]} seq-rdr rgidx & [options]]
   (let [chr (normalize-chromosome-key chr)]
     (if (valid-ref? seq-rdr chr pos ref)
       (->> (rg/ref-genes chr pos rgidx)
@@ -130,16 +163,21 @@
                                          seq-rdr rg)
                          :rg rg)))
            (filter #(cds-affected? % (:rg %)))
-           (keep #(prot/->hgvs % seq-rdr (:rg %)))
+           (keep (fn [{:keys [rg] :as m}]
+                   (when (:verbose? options)
+                     (print-debug-info m seq-rdr rg))
+                   (prot/->hgvs m seq-rdr rg)))
            distinct)
       (throw (Exception. (format "\"%s\" is not found on %s:%d" ref chr pos))))))
 
 (defmethod vcf-variant->protein-hgvs :ref-gene-entity
-  [{:keys [pos ref alt]} seq-rdr {:keys [chr] :as rg}]
+  [{:keys [pos ref alt]} seq-rdr {:keys [chr] :as rg} & [options]]
   (if (valid-ref? seq-rdr chr pos ref)
     (let [nv (select-variant {:chr chr, :pos pos, :ref ref, :alt alt}
                              seq-rdr rg)]
-      (if (cds-affected? nv rg)
+      (when (cds-affected? nv rg)
+        (when (:verbose? options)
+          (print-debug-info nv seq-rdr rg))
         (prot/->hgvs (assoc nv :rg rg) seq-rdr rg)))
     (throw (Exception. (format "\"%s\" is not found on %s:%d" ref chr pos)))))
 
@@ -156,7 +194,8 @@
   Options:
 
     :tx-margin  The length of transcription margin, up to a maximum of 10000,
-                default 5000."
+                default 5000.
+    :verbose?   Print debug information, default false."
   {:arglists '([variant ref-seq ref-gene]
                [variant ref-seq ref-gene options])}
   (fn [_ ref-seq ref-gene & _]
@@ -184,6 +223,8 @@
                                          seq-rdr rg)
                          :rg rg)))
            (map (fn [{:keys [rg] :as m}]
+                  (when (:verbose? options)
+                    (print-debug-info m seq-rdr rg))
                   {:cdna (cdna/->hgvs m seq-rdr rg)
                    :protein (if (cds-affected? m rg)
                               (prot/->hgvs m seq-rdr rg))}))
@@ -191,10 +232,13 @@
       (throw (Exception. (format "\"%s\" is not found on %s:%d" ref chr pos))))))
 
 (defmethod vcf-variant->hgvs :ref-gene-entity
-  [{:keys [pos ref alt]} seq-rdr {:keys [chr] :as rg} & _]
+  [{:keys [pos ref alt]} seq-rdr {:keys [chr] :as rg} & options]
   (if (valid-ref? seq-rdr chr pos ref)
     (let [nv (select-variant {:chr chr, :pos pos, :ref ref, :alt alt}
                              seq-rdr rg)]
+      (when (:verbose? options)
+        (print-debug-info nv seq-rdr rg))
+
       {:cdna (cdna/->hgvs nv seq-rdr rg)
        :protein (if (cds-affected? nv rg)
                   (prot/->hgvs nv seq-rdr rg))})
