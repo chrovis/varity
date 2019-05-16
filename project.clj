44b66589cc260e5b6aaf55883a790de554e685f2
alumi <alumi@users.noreply.github.com>
2019-05-16 11:05:48 +0900

    Update cljam to 0.7.1

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/tools.logging "0.4.1"]
                  [clj-hgvs "0.3.0"]
-                 [cljam "0.7.0"]
+                 [cljam "0.7.1"]
                  [org.apache.commons/commons-compress "1.18"]
                  [proton "0.1.7"]]
   :plugins [[lein-cloverage "1.0.13"]
