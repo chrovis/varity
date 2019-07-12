2fb8c38a2d65689b935d7201512e7a98f481e444
Toshiki Takeuchi <take@xcoo.jp>
2019-07-12 19:20:21 +0900

    Update lein-codox to 0.10.7

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [org.apache.commons/commons-compress "1.18"]
                  [proton "0.1.8"]]
   :plugins [[lein-cloverage "1.1.1"]
-            [lein-codox "0.10.5"]
+            [lein-codox "0.10.7"]
             [net.totakke/lein-libra "0.1.2"]]
   :test-selectors {:default (complement :slow)
                    :slow :slow
