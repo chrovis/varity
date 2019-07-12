28186f32b434d8a2ddf7c3ffa7673ac00b28a2d7
Toshiki Takeuchi <take@xcoo.jp>
2019-07-12 19:12:45 +0900

    Update lein-cloverage to 1.1.1

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [cljam "0.7.1"]
                  [org.apache.commons/commons-compress "1.18"]
                  [proton "0.1.8"]]
-  :plugins [[lein-cloverage "1.0.13"]
+  :plugins [[lein-cloverage "1.1.1"]
             [lein-codox "0.10.5"]
             [net.totakke/lein-libra "0.1.2"]]
   :test-selectors {:default (complement :slow)
