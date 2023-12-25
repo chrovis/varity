bd55f599cd5f120682e94f22c53a4dcd7fc5cb9b
Nobuaki Karasawa <nkarasawa@xcoo.jp>
2023-12-26 08:24:33 +0900

    chore: update change log

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject varity "0.9.4-SNAPSHOT"
+(defproject varity "0.9.4"
   :description "Variant translation library for Clojure"
   :url "https://github.com/chrovis/varity"
   :license {:name "Apache License, Version 2.0"
