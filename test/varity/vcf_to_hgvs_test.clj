99707f56293895479e14efcb0e30ccb1bdfbde8c
Nobuaki Karasawa <nkarasawa@xcoo.jp>
2025-03-19 21:23:16 +0900

    fix: add stop codon substitution conditional branch to protein-variant

--- test/varity/vcf_to_hgvs_test.clj
+++ test/varity/vcf_to_hgvs_test.clj
@@ -221,6 +221,7 @@
         "chr2" 47478341 "TG" "T" '("p.L762*" "p.L696*") ;; rs786204050 (+) frameshift with termination
         "chr17" 7676202 "T" "TGTCCCTTAGTCTT" '("p.P58*" "p.P19*") ; cf. not actual example (-, frameshift with termination)
         "chr13" 24421115 "TGACTTAGCC" "T" '("p.G1724K") ; not actual example (-, del includes termination codon)
+        "chrX" 15823233 "ATCCAAATAAAC" "A" '("p.S481*") ; not actual example (+, del includes termination codon)
 
         ;; deletion
         "chr1" 240092288 "AGTC" "A" '("p.S61del") ; cf. rs772088733 (+)
