4a31caaabdf4782b52bdee19730867210e43026d
Toshiki Takeuchi <take@xcoo.jp>
2025-03-25 11:50:07 +0900

    chore: add support for clojure 1.12

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :min-lein-version "2.7.0"
-  :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.12.0" :scope "provided"]
                  [org.clojure/tools.logging "1.2.4"]
                  [clj-hgvs "0.5.1"]
                  [cljam "0.8.3"]
@@ -23,7 +23,8 @@
              :repl {:source-paths ["bench"]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
-             :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}}
+             :1.11 {:dependencies [[org.clojure/clojure "1.11.3"]]}
+             :1.12 {:dependencies [[org.clojure/clojure "1.12.0"]]}}
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo/"
                                       :username [:env/clojars_username :gpg]
                                       :password [:env/clojars_password :gpg]}]]
