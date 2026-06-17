353d9e41c3296e6a92d912a6cc7ca1fd7b1aa97e
Niyarin <niyarium@gmail.com>
2026-06-17 12:55:14 +0900

    test: Add a test for chains that contains size=0 entry.

--- test/varity/chain_test.clj
+++ test/varity/chain_test.clj
@@ -23,42 +23,65 @@
    "chr3" [{:header {:t-name "chr2", :t-start 0, :t-end 300 :score 1}}]})
 
 (deftest search-chains-test
-  (let [chain [{:header
-                {:t-name "chr1" :q-name "chr1"
-                 :t-start 0 :q-start 0
-                 :t-end 26 :q-end 18
-                 :tsize 26 :q-size 18}
-                :data
-                [{:size 10 :dt 2 :dq 0}
-                 {:size 1 :dt 0 :dq 2}
-                 {:size 1 :dt 4 :dq 0}
-                 {:size 1 :dt 4 :dq 0}
-                 {:size 3}]}]
-        chain-idx (chain/index chain)]
-    (are [pos ans] (= (-> (chain/search-chains "chr1" pos chain-idx)
-                          first
-                          :in-block
-                          (select-keys [:q-start :t-start])
-                          not-empty)
-                      ans)
-      9 {:t-start 1 :q-start 1}
-      10 {:t-start 1 :q-start 1}
-      11 nil
-      12 nil
-      13 {:t-start 13 :q-start 11} ;;gap=+2
-      14 {:t-start 14 :q-start 14} ;;gap=+2-2
-      15  nil
-      16  nil
-      17  nil
-      18  nil
-      19 {:t-start 19 :q-start 15} ;;gap=+2-2+4
-      20  nil
-      21  nil
-      22  nil
-      23  nil
-      24  {:t-start 24 :q-start 16} ;;gap-+2-2+4+4
-      25  {:t-start 24 :q-start 16}
-      26  {:t-start 24 :q-start 16})))
+  (testing "simple test"
+    (let [chain [{:header
+                  {:t-name "chr1" :q-name "chr1"
+                   :t-start 0 :q-start 0
+                   :t-end 26 :q-end 18
+                   :tsize 26 :q-size 18}
+                  :data
+                  [{:size 10 :dt 2 :dq 0}
+                   {:size 1 :dt 0 :dq 2}
+                   {:size 1 :dt 4 :dq 0}
+                   {:size 1 :dt 4 :dq 0}
+                   {:size 3}]}]
+          chain-idx (chain/index chain)]
+      (are [pos ans] (= (-> (chain/search-chains "chr1" pos chain-idx)
+                            first
+                            :in-block
+                            (select-keys [:q-start :t-start])
+                            not-empty)
+                        ans)
+        9 {:t-start 1 :q-start 1}
+        10 {:t-start 1 :q-start 1}
+        11 nil
+        12 nil
+        13 {:t-start 13 :q-start 11} ;;gap=+2
+        14 {:t-start 14 :q-start 14} ;;gap=+2-2
+        15  nil
+        16  nil
+        17  nil
+        18  nil
+        19 {:t-start 19 :q-start 15} ;;gap=+2-2+4
+        20  nil
+        21  nil
+        22  nil
+        23  nil
+        24  {:t-start 24 :q-start 16} ;;gap-+2-2+4+4
+        25  {:t-start 24 :q-start 16}
+        26  {:t-start 24 :q-start 16})))
+  (testing "size=0 test"
+    (let [chain [{:header
+                  {:t-name "chr1" :q-name "chr1"
+                   :t-start 0 :q-start 0
+                   :t-end 21 :q-end 12
+                   :tsize 21 :q-size 12}
+                  :data
+                  [{:size 0 :dt 4 :dq 0} ;; [1,4]
+                   {:size 6 :dt 0 :dq 0} ;; [5,10]
+                   {:size 0 :dt 4 :dq 0} ;; [11,14]
+                   {:size 6}]}] ;;[15,20]
+          chain-idx (chain/index chain)]
+      (is
+       (= (map #(-> (chain/search-chains "chr1" % chain-idx)
+                    first
+                    :in-block
+                    some?)
+               (range 1 21))
+          [false false false false ;; [1,4]
+           true true true true true true ;; [5,10]
+           false false false false ;; [11, 14]
+           true true true true true true]))))) ;; [15,20]
 
 (deftest search-overlap-blocks-test
   (are [start end ans] (= (map #(select-keys % [:t-start :t-end])
